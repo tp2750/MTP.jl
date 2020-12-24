45cce003977c43103472b43b1f43a81227312ef7
Thomas A. Poulsen <thomas@lha66.dk>
2020-12-24 12:32:14 +0100

    define plates

--- src/MTP.jl
+++ src/MTP.jl
@@ -1,5 +1,47 @@
 module MTP
 
-# Write your package code here.
+import Base.@kwdef
+using Printf
+
+export Mtp96, Mtp384, wellname
+
+abstract type Mtp end
+
+struct Mtp96 <: Mtp
+    name::String
+    barcode::String
+    geometry::Int
+    well::Array{String}
+end
+
+struct Mtp384 <: Mtp
+    name::String
+    barcode::String
+    geometry::Int
+    well::Array{String}
+end
+
+
+function Mtp96(name::String; barcode="")
+    wells = vec(string.(repeat('A':'H',12),lpad.(repeat(1:12, outer=8),2,"0")))
+    Mtp96(name, barcode, 96, wells)
+end
+
+function Mtp384(name::String; barcode="")
+    wells = vec(string.(repeat('A':'P',24),lpad.(repeat(1:24, outer=16),2,"0")))
+    Mtp384(name, barcode, 384 , wells)
+end
+
+
+
+function wellname(w)
+    pat = r"(\D)(\d+)"
+    m = match(pat, w)
+    l = m[1]
+    ns = lpad(parse(Int,m[2]),2,"0")
+    "$l$ns"
+end
+
 
 end
+
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,10 @@ using MTP
 using Test
 
 @testset "MTP.jl" begin
-    # Write your tests here.
+    p1 = Mtp96("Plate 1")
+    @test p1.name == "Plate 1"
+    @test p1.barcode == ""
+    @test p1.well == wellname.(p1.well)
+    @test wellname.(["A1", "A12"]) == ["A01", "A12"]    
+    p2 = Mtp384("Plate 2")
 end
