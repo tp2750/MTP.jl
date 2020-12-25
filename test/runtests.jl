ef51ccc0d2f71508c62b0c9dbd10708596bc444c
Thomas A. Poulsen <thomas@lha66.dk>
2020-12-25 17:41:36 +0100

    test xlsx functions

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,4 @@
-using MTP
+using MTP, XLSX
 using Test
 
 @testset "MTP.jl" begin
@@ -8,4 +8,10 @@ using Test
     @test p1.well == wellname.(p1.well)
     @test wellname.(["A1", "A12"]) == ["A01", "A12"]    
     p2 = Mtp384("Plate 2")
+end
+@testset "XLSX" begin
+    wb = XLSX.readxlsx("plate_test_2.xlsx")
+    s1 = wb[1]
+    m1 = MTP.sheet_as_matrix(s1)
+    @test size(m1) == (25,13)
 end
